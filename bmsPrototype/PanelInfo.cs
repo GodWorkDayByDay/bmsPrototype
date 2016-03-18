a93196b8e40dd8e5938113d8c79131f0fde9fa9a
ktcsi <raize84093@gmail.com>
2016-03-18 22:58:47 +0900

    Fps and so on
    This PanelInfo is cheap now.

---
+++ bmsPrototype/PanelInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace bmsPrototype
+{
+    public class PanelInfo
+    {
+        public Vector2 Position;
+        public Image Base, Fps;
+        FrameCounter frameCounter;
+        ContentManager content;
+        SpriteFont font;
+        string fps, gameTime, eGameTime;
+
+        public PanelInfo()
+        {
+            Position = Vector2.Zero;
+            frameCounter = new FrameCounter();
+            fps = "0";
+            gameTime = eGameTime = string.Empty;
+        }
+
+        public void LoadContent()
+        {
+            Base.LoadContent();
+            Fps.LoadContent();
+            Base.Position = Position + Base.Position;
+            Fps.Position = Position + Fps.Position;
+
+            string FontName = Fps.FontName;
+            content = new ContentManager(SceneManager.Instance.Content.ServiceProvider, "Content");
+            font = content.Load<SpriteFont>(FontName);
+        }
+
+        public void UnloadContent()
+        {
+            Base.UnloadContent();
+            Fps.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            /* some of fps counter */
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            frameCounter.Update(deltaTime);
+            fps = string.Format("FPS: {0}", frameCounter.AverageFramesPerSecond);
+            this.gameTime = "TGT: " + gameTime.TotalGameTime.ToString();
+            this.eGameTime = "EGT: " + gameTime.ElapsedGameTime.ToString();
+            /* Update */
+            Base.Update(gameTime);
+            Fps.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            /* Draw */
+            Base.Draw(spriteBatch);
+            //Fps.Draw(spriteBatch);
+            spriteBatch.DrawString(font, fps, Fps.Position, Color.White);
+            spriteBatch.DrawString(font, gameTime, new Vector2(Fps.Position.X, Fps.Position.Y + Fps.Texture.Height * 1), Color.White);
+            spriteBatch.DrawString(font, eGameTime, new Vector2(Fps.Position.X, Fps.Position.Y + Fps.Texture.Height * 2), Color.White);
+        }
+    }
+}
--- bmsPrototype/ScenePlayBMS.cs
+++ bmsPrototype/ScenePlayBMS.cs
@@ -12,31 +12,39 @@ namespace bmsPrototype
     public class ScenePlayBMS : Scene
     {
         PanelPlayer PanelPlayer;
+        PanelInfo PanelInfo;
 
         public override void LoadContent()
         {
             base.LoadContent();
             xmlManager<PanelPlayer> panelPlayerLoader = new xmlManager<PanelPlayer>();
             PanelPlayer = panelPlayerLoader.Load("Load/PlayBMS/PanelPlayer.xml");
+            xmlManager<PanelInfo> panelInfoLoader = new xmlManager<PanelInfo>();
+            PanelInfo = panelInfoLoader.Load("Load/PlayBMS/PanelInfo.xml");
+            /* Load Content */
             PanelPlayer.LoadContent();
+            PanelInfo.LoadContent();
         }
 
         public override void UnloadContent()
         {
             base.UnloadContent();
             PanelPlayer.UnloadContent();
+            PanelInfo.UnloadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
             PanelPlayer.Update(gameTime);
+            PanelInfo.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
             PanelPlayer.Draw(spriteBatch);
+            PanelInfo.Draw(spriteBatch);
         }
     }
 }
